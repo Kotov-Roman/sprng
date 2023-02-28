f280f2b2059c60450fb537091058549291493078
Roman_Kotov <roma_kotovo@tr.com>
2023-02-28 16:29:38 +0100

    Create bare spring-boot app

---
+++ README.md
@@ -0,0 +1,8 @@
+# Backend services program
+Spring demo app.
+
+Technologies/tools being used:
+1. Spring Boot 2.7.6
+2. Spring Data
+3. Maven
+
---
+++ src/test/java/com/backend/sprng/SprngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sprng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
